f3003a008bcf2903725c8899302d54050dce7117
Guiselle Tatiana Zambrano Penagos <gtzambranop@unal.edu.co>
2021-04-09 19:27:16 -0500

    pandas: q09, q10 modified

--- pandas/q09/solution.py
+++ pandas/q09/solution.py
@@ -11,18 +11,24 @@ def formatList(my_list):
     return separator.join(my_list)
 
 def main():
-	# leyendo los datos del archivo 'tbl1.tsv', columnas por '\t'
-	data = pd.read_csv('tbl1.tsv', sep='\t')
+    # leyendo los datos del archivo 'tbl1.tsv', columnas por '\t'
+    data = pd.read_csv('tbl1.tsv', sep='\t')
 
-	# Agrupar columna '_c2' por una específica, en este caso '_c1', luego
-	# a la columna '_c2' se le aplica una función que transforma en una lista
-	# los elementos relacionados de '_c2' con cada caracter de '_c1'
-	data = data.groupby('_c0')['_c4'].apply(list).reset_index(name='lista')
+    # Agrupar columna '_c2' por una específica, en este caso '_c1', luego
+    # a la columna '_c2' se le aplica una función que transforma en una lista
+    # los elementos relacionados de '_c2' con cada caracter de '_c1'
+    data = data.groupby('_c0')['_c4'].apply(list).reset_index(name='lista')
 
-	# La lista generada no está ordenada ni en el formato solicitado, por lo que
-	# debe ser modificada usando la función "formatList()"
-	data['lista'] = [formatList(l) for l in data['lista']]
-	print(data)
+    temp = list()
+    for my_list in data['lista']:
+        separator = ','
+        # Se ordena la lista
+        my_list.sort()
+        # Se retorna un string, donde están los valores de la lista "l"
+        # separados por ':'
+        temp.append(separator.join(my_list))
+    data['lista'] = temp
+    print(data)
 
 if __name__ == "__main__":
     main()
--- pandas/q10/solution.py
+++ pandas/q10/solution.py
@@ -1,35 +1,35 @@
 # Librería de pandas con sobrenombre 'pd'
 import pandas as pd
 
-# Organiza cada lista en el formato solicitado
-def formatList(my_list):
-    separator = ','
-    # Se ordena la lista
-    my_list.sort()
-    # Se retorna un string, donde están los valores de la lista "l"
-    # separados por ':'
-    return separator.join(my_list)
-
 def main():
-	# leyendo los datos del archivo 'tbl2.tsv', columnas por '\t'
-	data = pd.read_csv('tbl2.tsv', sep='\t')
+    # leyendo los datos del archivo 'tbl2.tsv', columnas por '\t'
+    data = pd.read_csv('tbl2.tsv', sep='\t')
 
-	# Se recorren 2 columnas y se crea una lista, donde el primer y segundo elemento
-	# 'a' y 'b', pertenecientes a '_c5a' y '_c5b' respectivamente
-	# son organizadas de la forma 'a:b'
-	# esta lista es asignada a una nueva columna llamada 'temp'
-	data['temp'] = ['{}:{}'.format(a,b) for a, b in zip(data['_c5a'], data['_c5b'])]
+    # Se recorren 2 columnas y se crea una lista, donde el primer y segundo elemento
+    # 'a' y 'b', pertenecientes a '_c5a' y '_c5b' respectivamente
+    # son organizadas de la forma 'a:b'
+    # esta lista es asignada a una nueva columna llamada 'temp'
+    data['temp'] = ['{}:{}'.format(a,b) for a, b in zip(data['_c5a'], data['_c5b'])]
 
-	# Agrupar columna 'temp' por una específica, en este caso '_c0', luego
-	# a la columna 'temp' se le aplica una función que transforma en una lista
-	# los elementos relacionados de 'temp' para cada número de '_c0'
-	data = data.groupby('_c0')['temp'].apply(list).reset_index(name='lista')
+    # Agrupar columna 'temp' por una específica, en este caso '_c0', luego
+    # a la columna 'temp' se le aplica una función que transforma en una lista
+    # los elementos relacionados de 'temp' para cada número de '_c0'
+    data = data.groupby('_c0')['temp'].apply(list).reset_index(name='lista')
 
-	# La lista generada no está ordenada ni en el formato solicitado, por lo que
-	# debe ser modificada usando la función "formatList()"
-	data['lista'] = [formatList(l) for l in data['lista']]
+    temp = list()
 
-	print(data)
+    for my_list in data['lista']:
+        separator = ','
+        # Se ordena la lista
+        my_list.sort()
+        # Se retorna un string, donde están los valores de la lista "l"
+        # separados por ':'
+        temp.append(separator.join(my_list))
+
+    #data['lista'] = [formatList(l) for l in data['lista']]
+    data['lista'] = temp
+
+    print(data)
 
 if __name__ == "__main__":
     main()
